8cc1c595a84aa7b80bab0abbe7e37692d93ff448
Jarrell Houston <jhouston2882@yahoo.com>
2021-01-05 17:30:00 -0600

    working on updating account

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import AccountOption from './AccountOption';
 import '../css/Account.css';
+
 import { useStateValue } from '../StateProvider';
+import { Link } from 'react-router-dom';
 
 function Account() {
     const [{ user }, dispatch] = useStateValue();
@@ -16,13 +18,13 @@ function Account() {
                 <h1>Your Account</h1>
 
                 <div className="account__info">
-                    <AccountOption title={"Name"} value={user.displayName ? user.displayName : "Name not set"} style={{ borderTopLeftRadius: "7px", borderTopRightRadius: "7px", borderTop: "1px solid lightgray" }} />
+                    <AccountOption title={"Name"} value={user?.displayName ? user?.displayName : "Name not set"} style={{ borderTopLeftRadius: "7px", borderTopRightRadius: "7px", borderTop: "1px solid lightgray" }} />
                     <AccountOption title={"Email"} value={user.email ? user.email : "Email not set"} />
-                    <AccountOption title={"Phone Number"} value={user.phoneNumber ? user.phoneNumber : "Phone Number not set"} />
+                    <AccountOption title={"Phone Number"} value={user?.phoneNumber ? user?.phoneNumber : "Phone Number not set"} />
                     <AccountOption title={"Password"} value={"*********"} style={{ borderBottomLeftRadius: "7px", borderBottomRightRadius: "7px" }} />
                 </div>
 
-                <button className="account__doneButton">Done</button>
+                <Link to="/"><button className="account__doneButton">Done</button></Link>
             </div>
 
         </div>
--- src/components/AccountOption.js
+++ src/components/AccountOption.js
@@ -1,16 +1,100 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/AccountOption.css';
 
+import { auth } from '../firebase';
+import { useStateValue } from '../StateProvider';
+
 function AccountOption({ title, value, style }) {
+    const [{ user }, dispatch] = useStateValue();
+    const [update, setUpdate] = useState(false);
+    const [input, setInput] = useState(value);
+
+    const inputRef = React.createRef();
+
+    useEffect(() => {
+        if (update) {
+            inputRef.current.focus();
+        }
+    }, [update])
+
+    const updateItem = () => {
+        switch (title) {
+            case "Name":
+                if (input) {
+                    auth.currentUser.updateProfile({
+                        displayName: input
+                    }).then(() => {
+                        dispatch({
+                            type: 'SET_USER',
+                            user: auth.currentUser
+                        })
+                        setUpdate(false);
+
+                        alert("Successfully updated Name");
+                    })
+                } else {
+                    alert("Please insert Name value");
+                }
+
+            case "Email":
+                if (input) {
+                    auth.currentUser.updateEmail(input)
+                        .then(() => {
+                            dispatch({
+                                type: 'SET_USER',
+                                user: auth.currentUser
+                            })
+                            setUpdate(false);
+
+                            alert("Successfully updated Email");
+                        })
+                } else {
+                    alert("Please insert valid Email")
+                }
+
+            case "Phone Number":
+                if (input) {
+                    auth.currentUser.updatePhoneNumber(input)
+                        .then(() => {
+                            dispatch({
+                                type: 'SET_USER',
+                                user: auth.currentUser
+                            })
+                            setUpdate(false);
+
+                            alert("Successfully updated Phone Number");
+
+                        })
+                } else {
+                    alert("Please insert valid Phone Number")
+                }
+
+            default:
+                break;
+        }
+
+
+    }
 
     return (
         <div className="accountOption" style={style}>
             <div className="accountOption__container">
-                <div className="accountInfo">
+                <div className="accountOption__info">
                     <h3>{title}:</h3>
-                    <p>{value}</p>
+                    {!update ?
+                        <p>{value}</p>
+                        :
+                        <input type="text" value={input} className="accountOption__input" onChange={(e) => { setInput(e.target.value) }} ref={inputRef} />
+                    }
                 </div>
-                <button className="accountEdit">Edit</button>
+
+                {!update ?
+                    <button className="accountOption__edit" onClick={() => { setUpdate(true) }}>Edit</button>
+
+                    :
+
+                    <button className="accountOption__update" onClick={updateItem}>Update</button>
+                }
             </div>
         </div>
     )
